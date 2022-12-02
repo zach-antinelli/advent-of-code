2733cefca42b8c7234242d358e2415bb803e924a
Zach Antinelli <zach.antinelli@tanium.com>
2022-12-01 16:33:09 -0800

    day 1 solution cd /Users/z/Documents/git/AoC/2022/01 ; /usr/bin/env /usr/local/bin/python3 /Users/z/.vscode/extensions/ms-python.python-2022.18.2/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 50903 -- /Users/z/Documents/git/AoC/2022/01/01.py

---
+++ 2022/01/01.py
@@ -0,0 +1,19 @@
+INPUT = open('input.txt').read()
+
+def findTheFatElf(input):
+    inputList = input.split('\n')
+    results = []
+    total = 0
+    for i in inputList:
+        if i == '':
+            results.append(total)
+            total = 0
+            continue
+        total = total + int(i)
+    results.sort()
+    print('Most calories held by one elf: ' + str(results[-1]))
+    print('Calories held by top three elves combined: ' + str(results[-1] + results[-2] + results [-3]))
+
+
+findTheFatElf(INPUT)
+INPUT.close()
