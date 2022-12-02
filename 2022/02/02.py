f57f371f0c38d5041965eefb555ef3a3c13299a2
Zach Antinelli <zach.antinelli@tanium.com>
2022-12-01 23:29:43 -0800

    challenge completed

---
+++ 2022/02/02.py
@@ -0,0 +1,31 @@
+INPUT = open('input.txt').read().splitlines()
+
+def partOne(inputStrategy):
+    total = 0
+    for i in INPUT:
+        op, me = i.split()
+        total += {'X': 1,'Y': 2, 'Z': 3}[me]
+        total += {  ('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0,
+                    ('B', 'Y'): 3, ('B', 'Z'): 6, ('B', 'X'): 0,
+                    ('C', 'Z'): 3, ('C', 'X'): 6, ('C', 'Y'): 0  }[(op, me)]
+    return total
+
+print('Total number of points for part one:', partOne(INPUT))
+
+def partTwo(inputStrategy):
+    total = 0
+    for i in INPUT:
+        op, me = i.split()
+        total += {'X': 0,'Y': 3, 'Z': 6}[me]
+        total += {  ('A', 'X'): 3, ('A', 'Y'): 1, ('A', 'Z'): 2,
+                    ('B', 'Y'): 2, ('B', 'Z'): 3, ('B', 'X'): 1,
+                    ('C', 'Z'): 1, ('C', 'X'): 2, ('C', 'Y'): 3  }[(op, me)]
+    return total
+
+print('Total number of points for part two:', partTwo(INPUT))
+
+
+    
+
+
+
